bdf6e5821c38cb96669ed2bca3fb5320734e7610
Priyanka Bhat <bhat2601@gmail.com>
2021-11-21 12:24:27 +0530

    Third Commit

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
-from logging import root
-from tkinter import Label
-
 from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
-#conn = sqlite3.connect("project.db")
-#c = conn.cursor()
-#c.execute("DROP TABLE IF EXISTS customer_details")
-#c.execute("CREATE TABLE  customer_details(firstname VARCHAR(45), middlename VARCHAR(45), lastname VARCHAR(45), cpf_no VARCHAR(45), DOB DATE, phonenumber CHAR(10), emailid VARCHAR(50), DOJ DATE, DOE DATE, total_share_amt INT, total_lt_out INT, total_st_out INT)")
-#conn.commit()
-#conn.close()
 
+
+# conn = sqlite3.connect("project.db") c = conn.cursor() c.execute("DROP TABLE IF EXISTS customer_details")
+# c.execute("CREATE TABLE  customer_details(firstname VARCHAR(45), middlename VARCHAR(45), lastname VARCHAR(45),
+# cpf_no VARCHAR(45), DOB DATE, phonenumber CHAR(10), emailid VARCHAR(50), DOJ DATE, DOE DATE, total_share_amt INT,
+# total_lt_out INT, total_st_out INT)") conn.commit() conn.close()
 
 @app.route("/homepage")
 def homepage():
     return render_template("homepage.html")
 
 
+# New Loan
+@app.route("/new_loan", methods=["GET", "POST"])
+def loan():
+    msg = ""
+    if request.method == "POST":
+        if request.form["loan_num"] != "" and request.form["loan_type"] != "" and request.form["loan_amt"] != "" and \
+                request.form["int_rate"] != "" and request.form["principal_amt"] != "" and request.form["no_int"] != "" and \
+                request.form["out_loan_amt"] != "" and request.form["cust_id"] != "" and request.form["loan_disb_date"] != "":
+            loan_num = request.form["loan_num"]
+            loan_type = request.form["loan_type"]
+            loan_amt = request.form["loan_amt"]
+            int_rate = request.form["int_rate"]
+            principal_amt = request.form["principal_amt"]
+            no_int = request.form["no_int"]
+            out_loan_amt = request.form["out_loan_amt"]
+            cust_id = request.form["cust_id"]
+            loan_disb_date = request.form["loan_disb_date"]
+            conn = sqlite3.connect("project.db")
+            c = conn.cursor()
+            c.execute(
+                "INSERT INTO loan_details VALUES('" +  loan_num + "', '" +  loan_type + "' , '" +  loan_amt + "', '" +  int_rate + "', '" + principal_amt + "',  '" + no_int + "', '" + out_loan_amt + "', '" + cust_id + "',  '" + loan_disb_date + "')")
+            msg = "Your account is created"
+            conn.commit()
+            conn.close()
+
+    return render_template("new_loan.html", msg=msg)
+
+
+# Customer Details
 @app.route("/customer_details", methods=["GET", "POST"])
 def details():
     msg = ""
     if request.method == "POST":
-        if request.form["firstname"] != "" and request.form["middlename"] != "" and request.form["lastname"] != "" and request.form["cpf_no"] != "" and request.form["DOB"] != "" and request.form["phonenumber"] != "" and  request.form["emailid"] != "" and request.form["DOJ"] != "" and request.form["DOE"] != "" and request.form["total_share_amt"] != "" and request.form["total_lt_out"] != "" and request.form["total_st_out"] != "":
+        if request.form["firstname"] != "" and request.form["middlename"] != "" and request.form["lastname"] != "" and \
+                request.form["cpf_no"] != "" and request.form["DOB"] != "" and request.form["phonenumber"] != "" and \
+                request.form["emailid"] != "" and request.form["DOJ"] != "" and request.form["DOE"] != "" and \
+                request.form["total_share_amt"] != "" and request.form["total_lt_out"] != "" and request.form["total_st_out"] != "":
             firstname = request.form["firstname"]
             middlename = request.form["middlename"]
             lastname = request.form["lastname"]
@@ -39,22 +67,22 @@ def details():
             conn = sqlite3.connect("project.db")
             c = conn.cursor()
             c.execute(
-                 "INSERT INTO customer_details VALUES('" + firstname + "', '" + middlename + "' , '" + lastname + "', '" + cpf_no + "', '" +  DOB  + "',  '" + phonenumber + "', '"+ emailid +"', '" +  DOJ + "',  '" +  DOE + "','" + total_share_amt + "', '" +  total_lt_out + "',  '" +  total_st_out + "')")
+                "INSERT INTO customer_details VALUES('" + firstname + "', '" + middlename + "' , '" + lastname + "', '" + cpf_no + "', '" + DOB + "',  '" + phonenumber + "', '" + emailid + "', '" + DOJ + "',  '" + DOE + "','" + total_share_amt + "', '" + total_lt_out + "',  '" + total_st_out + "')")
             msg = "Your account is created"
             conn.commit()
             conn.close()
 
-    return render_template("customer_details.html",msg=msg)
+    return render_template("customer_details.html", msg=msg)
 
 
-@app.route("/customer_details/search",methods=["GET", "POST"])
+@app.route("/customer_details/search", methods=["GET", "POST"])
 def search():
     if request.method == "POST":
         cpf_no = request.form["cpf_no"]
         conn = sqlite3.connect("project.db")
         conn.row_factory = sqlite3.Row
         c = conn.cursor()
-        c.execute("select * from customer_details WHERE cpf_no = '"+cpf_no+"'")
+        c.execute("select * from customer_details WHERE cpf_no = '" + cpf_no + "'")
         rows = c.fetchall()
         c.close()
         for r in rows:
@@ -63,11 +91,14 @@ def search():
         return render_template("customer_details.html", rows=rows)
 
 
+# Register and Login page
 @app.route("/register", methods=["GET", "POST"])
 def register():
     msg = ""
     if request.method == "POST":
-        if request.form["userid"] != "" and request.form["emailid"] != "" and request.form["password"] != "" and request.form["firstname"] != "" and request.form["lastname"] != "" and request.form["status"] != "" and request.form["types"] != ""  and request.form["doj"] != ""   and  request.form["phonenumber"] != "" :
+        if request.form["userid"] != "" and request.form["emailid"] != "" and request.form["password"] != "" and \
+                request.form["firstname"] != "" and request.form["lastname"] != "" and request.form["status"] != "" and \
+                request.form["types"] != "" and request.form["doj"] != "" and request.form["phonenumber"] != "":
             userid = request.form["userid"]
             emailid = request.form["emailid"]
             password = request.form["password"]
@@ -79,7 +110,8 @@ def register():
             phonenumber = request.form["phonenumber"]
             conn = sqlite3.connect("project.db")
             c = conn.cursor()
-            c.execute("INSERT INTO user_login VALUES('" + userid + "', '"+ emailid + "' , '" + password + "', '"+ firstname +"', '"+ lastname +"', '"+ status +"', '"+ types +"',  '"+ doj +"',  '"+ phonenumber +"')")
+            c.execute(
+                "INSERT INTO user_login VALUES('" + userid + "', '" + emailid + "' , '" + password + "', '" + firstname + "', '" + lastname + "', '" + status + "', '" + types + "',  '" + doj + "',  '" + phonenumber + "')")
             msg = "Your account is created"
             conn.commit()
             conn.close()
@@ -96,7 +128,8 @@ def login():
         password = request.form["password"]
         conn = sqlite3.connect("project.db")
         c = conn.cursor()
-        c.execute("SELECT userid, emailid, password FROM user_login WHERE userid = '"+userid+"' and emailid = '"+emailid+"' and password = '"+password+"'")
+        c.execute(
+            "SELECT userid, emailid, password FROM user_login WHERE userid = '" + userid + "' and emailid = '" + emailid + "' and password = '" + password + "'")
         r = c.fetchall()
         for i in r:
             if userid == i[0] and emailid == i[1] and password == i[2]:
